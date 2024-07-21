b2f83d920269a332ac9c8c369c6bd1e1972dcbd9
mr.javaria.y2b@gmail.com <javaria.y2b@gmail.com>
2024-07-21 21:55:16 +0500

    quiz usestate

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import ProgressBar from "@/components/ProgressBar";
+import { ChevronLeft, X } from "lucide-react";
 
 const questions = [
   {
@@ -138,6 +139,10 @@ const questions = [
 export default function Home() {
   const [started, setStarted] = useState(false);
   const [currentQuestion, setcurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+
 
   const handleNext = () => {
     if (!started) {
@@ -149,8 +154,33 @@ export default function Home() {
       setcurrentQuestion(currentQuestion + 1);
     }
   };
+
+
+  const handleAnswer = (answer) => {
+    setSelectedAnswer(answer.id);
+    const isCurrentCorrect = answer.isCorrect;
+    if(isCurrentCorrect){
+      setScore(score + 1);
+
+    }
+    setIsCorrect(isCurrentCorrect);
+  }
+
+
+
   return (
     <div className="flex flex-col flex-1">
+      <div className="position-sticky top-0 z-10 shadow-md py-4 w-full">
+        <header className="grid grid-cols-[auto,1fr,auto] grid-flow-col items-center justify-between py-2 gap-2">
+          <Button size={"icon"} variant={"outline"}>
+            <ChevronLeft />
+          </Button>
+          <ProgressBar value={(currentQuestion / questions.length) * 100} />
+          <Button size={"icon"} variant={"outline"}>
+            <X />
+          </Button>
+        </header>
+      </div>
       <main className="flex justify-center flex-1">
         {!started ? (
           <h1 className="text-3xl font-bold">Welcome to the quizz page</h1>
@@ -162,10 +192,7 @@ export default function Home() {
             <div className="grid grid-cols-1 gap-6 mt-6">
               {questions[currentQuestion].answers.map((answer) => {
                 return (
-                  <Button
-                    key={answer.id}
-                    variant={"secondary"}
-                  >
+                  <Button key={answer.id} variant={"secondary"} onClick={()=> handleAnswer(answer)}>
                     {answer.answerText}
                   </Button>
                 );
@@ -175,6 +202,7 @@ export default function Home() {
         )}{" "}
       </main>
       <footer className="footer pb-9 px-6 relative mb-0">
+        <p>{isCorrect ? 'correct': 'incorrect'}</p>
         <Button onClick={handleNext}>{!started ? "Start" : "Next"}</Button>
       </footer>
     </div>
--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-    value:number
-}
+  value: number;
+};
 
 const ProgressBar = (props: Props) => {
   return (
-    <div className='w-full bg-secondary rounded-full h-2.5'>
-        <div className='bg-primary h-2.5 rounded-md' style={{
-            width:`${props.value}`
-        }}>
-
-        </div>
+    <div className="w-full bg-secondary rounded-full h-2.5">
+      <div
+        className="bg-primary h-2.5 rounded-md"
+        style={{
+          width: `${props.value}%`,
+        }}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
